060851b3d01df6f0fb501ab269be146e46c3711c
Raghav Gupta <raghav.a.gupta@in.ibm.com>
2020-03-19 18:10:30 +0530

    added chnages

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>Raghav, helloworld!!!</div>
+      <div>Hello, helloworld!!!</div>
     );
   }
 }
